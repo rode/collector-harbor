1ef5be9b0c8aecb61c341a3bdd0f78fc5ead61bd
Michael Parker <michael@parker.gg>
2021-01-18 18:24:03 -0600

    fix config, add more config tests

--- config/config.go
+++ config/config.go
@@ -8,28 +8,28 @@ type Config struct {
 	Port         int
 	Debug        bool
 	RodeHost     string
-  HarborConfig *HarborConfig
+	HarborConfig *HarborConfig
 }
 
 type HarborConfig struct {
 	Host     string
-  Username string
-  Password string
+	Username string
+	Password string
 }
 
 func Build(name string, args []string) (*Config, error) {
 	flags := flag.NewFlagSet(name, flag.ContinueOnError)
 
-  c := &Config{
-    HarborConfig: &HarborConfig{},
-  }
+	c := &Config{
+		HarborConfig: &HarborConfig{},
+	}
 
-	flag.IntVar(&c.Port, "port", 8080, "the port that the harbor collector should listen on")
-	flag.BoolVar(&c.Debug, "debug", false, "when set, debug mode will be enabled")
-	flag.StringVar(&c.RodeHost, "rode-host", "rode:50051", "the host to use to connect to rode")
-	flag.StringVar(&c.HarborConfig.Host, "harbor-host", "http://harbor-harbor-core", "the host to use when contacting the Harbor API")
-	flag.StringVar(&c.HarborConfig.Username, "harbor-username", "", "The username to use to authenticate to Harbor")
-	flag.StringVar(&c.HarborConfig.Password, "harbor-password", "", "The password to use to authenticate to Harbor")
+	flags.IntVar(&c.Port, "port", 8080, "the port that the harbor collector should listen on")
+	flags.BoolVar(&c.Debug, "debug", false, "when set, debug mode will be enabled")
+	flags.StringVar(&c.RodeHost, "rode-host", "rode:50051", "the host to use to connect to rode")
+	flags.StringVar(&c.HarborConfig.Host, "harbor-host", "http://harbor-harbor-core", "the host to use when contacting the Harbor API")
+	flags.StringVar(&c.HarborConfig.Username, "harbor-username", "", "The username to use to authenticate to Harbor")
+	flags.StringVar(&c.HarborConfig.Password, "harbor-password", "", "The password to use to authenticate to Harbor")
 
 	err := flags.Parse(args)
 	if err != nil {
--- config/config_test.go
+++ config/config_test.go
@@ -15,6 +15,18 @@ func TestConfig(t *testing.T) {
 		expectError bool
 	}{
 		{
+			name:  "defaults",
+			flags: []string{},
+			expected: &Config{
+				Port:     8080,
+				Debug:    false,
+				RodeHost: "rode:50051",
+				HarborConfig: &HarborConfig{
+					Host: "http://harbor-harbor-core",
+				},
+			},
+		},
+		{
 			name:        "bad port",
 			flags:       []string{"--port=foo"},
 			expectError: true,
@@ -25,23 +37,27 @@ func TestConfig(t *testing.T) {
 			expectError: true,
 		},
 		{
-			name:        "Rode host",
-			flags:       []string{"--rode-host=bar"},
-      expected: &Config{
-				Port:  8080,
-				Debug: false,
-        RodeHost: "bar",
-        HarborHost: "http://harbor-harbor-core",
+			name:  "Rode host",
+			flags: []string{"--rode-host=bar"},
+			expected: &Config{
+				Port:     8080,
+				Debug:    false,
+				RodeHost: "bar",
+				HarborConfig: &HarborConfig{
+					Host: "http://harbor-harbor-core",
+				},
 			},
 		},
 		{
-			name:        "Harbor host",
-			flags:       []string{"--harbor-host=foo"},
-      expected: &Config{
-				Port:  8080,
-				Debug: false,
-        RodeHost: "rode:50051",
-        HarborHost: "foo".
+			name:  "Harbor host",
+			flags: []string{"--harbor-host=foo"},
+			expected: &Config{
+				Port:     8080,
+				Debug:    false,
+				RodeHost: "rode:50051",
+				HarborConfig: &HarborConfig{
+					Host: "foo",
+				},
 			},
 		},
 	} {
@@ -50,7 +66,7 @@ func TestConfig(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			c, err := Build("rode", tc.flags)
+			c, err := Build("rode-collector-harbor", tc.flags)
 
 			if tc.expectError {
 				Expect(err).To(HaveOccurred())
