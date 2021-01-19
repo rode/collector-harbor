70c43db315a19d1d5663c0896fa7b8f1584e4851
Michael Parker <michael@parker.gg>
2021-01-19 13:36:23 -0600

    add harbor client to harbor package (#5)

---
+++ harbor/client.go
@@ -0,0 +1,86 @@
+package harbor
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/rode/collector-harbor/config"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Client interface {
+	GetArtifacts(project, repository string) ([]*Artifact, error)
+	GetArtifactReport(project, repository, tag string) (*Report, error)
+}
+
+type client struct {
+	harborConfig   *config.HarborConfig
+	basicAuthToken string
+	httpClient     *http.Client
+}
+
+func NewClient(harborConfig *config.HarborConfig) Client {
+	c := &client{
+		harborConfig: harborConfig,
+		httpClient: &http.Client{
+			Timeout: time.Second * 10,
+		},
+	}
+
+	if harborConfig.Username != "" && harborConfig.Password != "" {
+		c.basicAuthToken = base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", harborConfig.Username, harborConfig.Password)))
+	}
+
+	return c
+}
+
+func (c *client) GetArtifacts(project, repository string) ([]*Artifact, error) {
+	var artifacts []*Artifact
+
+	uri := fmt.Sprintf("/api/v2.0/projects/%s/repositories/%s/artifacts", project, repository)
+
+	err := c.get(uri, &artifacts)
+	if err != nil {
+		return nil, err
+	}
+
+	return artifacts, nil
+}
+
+func (c *client) GetArtifactReport(project, repository, tag string) (*Report, error) {
+	var scanOverview ScanOverview
+
+	uri := fmt.Sprintf("/api/v2.0/projects/%s/repositories/%s/artifacts/%s/additions/vulnerabilities", project, repository, tag)
+
+	err := c.get(uri, &scanOverview)
+	if err != nil {
+		return nil, err
+	}
+
+	return scanOverview.Report, nil
+}
+
+func (c *client) get(uri string, resource interface{}) error {
+	req, err := http.NewRequest(http.MethodGet, c.harborConfig.Host+uri, nil)
+	if err != nil {
+		return err
+	}
+
+	if c.basicAuthToken != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("Basic %s", c.basicAuthToken))
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, resource)
+}
--- harbor/types.go
+++ harbor/types.go
@@ -2,5 +2,101 @@ package harbor
 
 // Event received
 type Event struct {
-	ID string `json:"id"`
+	Type     WebhookEvent `json:"type"`
+	OccurAt  int64        `json:"occur_at"`
+	Operator string       `json:"operator"`
+	Data     *EventData   `json:"event_data"`
 }
+
+// Vulnerability is...
+type EventData struct {
+	Resources  []*Resource `json:"resources"`
+	Repository *Repository `json:"repository"`
+}
+
+// Repository is...
+type Repository struct {
+	DateCreated  int    `json:"date_created"`
+	Name         string `json:"name"`
+	Namespace    string `json:"namespace"`
+	RepoFullName string `json:"repo_full_name"`
+	RepoType     string `json:"repo_type"`
+}
+
+// Resource is...
+type Resource struct {
+	Digest       string        `json:"digest"`
+	Tag          string        `json:"tag"`
+	ResourceUrl  string        `json:"resource_url"`
+	ScanOverview *ScanOverview `json:"scan_overview"`
+}
+
+// ScanOverview is...
+type ScanOverview struct {
+	Report *Report `json:"application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0"`
+}
+
+// is...
+type Report struct {
+	ReportId           string           `json:"report_id"`
+	ScanStatus         string           `json:"scan_status"`
+	Severity           string           `json:"severity"`
+	Duration           int              `json:"duration"`
+	Summary            *Summary         `json:"summary"`
+	StartTime          string           `json:"start_time"`
+	EndTime            string           `json:"end_time"`
+	Scanner            *Scanner         `json:"scanner"`
+	CompletePercentage int              `json:"complete_percent"`
+	Vulnerabilities    []*Vulnerability `json:"vulnerabilities"`
+}
+
+// Summary is...
+type Vulnerability struct {
+	ArtifactDigest string   `json:"artifact_digest"`
+	Description    string   `json:"description"`
+	FixVersion     string   `json:"fix_version"`
+	ID             string   `json:"id"`
+	Links          []string `json:"links"`
+	Package        string   `json:"package"`
+	Severity       string   `json:"severity"`
+	Version        string   `json:"version"`
+}
+
+// Summary is...
+type Summary struct {
+	Total   int    `json:"total"`
+	Fixable int    `json:"fixable"`
+	Summary *Count `json:"summary"`
+}
+
+// Count is...
+type Count struct {
+	Critical int `json:"Critical"`
+	High     int `json:"High"`
+	Low      int `json:"Low"`
+	Medium   int `json:"Medium"`
+}
+
+// Scanner is...
+type Scanner struct {
+	Name    string `json:"name"`
+	Vendor  string `json:"vendor"`
+	Version string `json:"version"`
+}
+
+type Artifact struct {
+	Tags   []*Tag `json:"tags"`
+	Digest string `json:"digest"`
+}
+
+type Tag struct {
+	Name string `json:"name"`
+}
+
+type WebhookEvent string
+
+const (
+	PUSH_ARTIFACT      WebhookEvent = "PUSH_ARTIFACT"
+	SCANNING_FAILED    WebhookEvent = "SCANNING_FAILED"
+	SCANNING_COMPLETED WebhookEvent = "SCANNING_COMPLETED"
+)
