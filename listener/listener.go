698edf50cd1ab829e6f88d49dccb4d49fb9da35c
Michael Parker <michael@parker.gg>
2021-01-20 18:00:32 -0600

    increase timeout for batchcreateoccurrences call

--- listener/listener.go
+++ listener/listener.go
@@ -75,7 +75,7 @@ func (l *listener) ProcessEvent(w http.ResponseWriter, request *http.Request) {
 		}
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	response, err := l.rodeClient.BatchCreateOccurrences(ctx, &pb.BatchCreateOccurrencesRequest{
