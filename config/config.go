e0eab317a8d1db37126f8876e9d1f89f52ff57c0
Michael Parker <michael@parker.gg>
2021-07-12 17:09:56 -0500

    switch to flags-first for parsing command line flags

--- config/config.go
+++ config/config.go
@@ -16,6 +16,7 @@ package config
 
 import (
 	"flag"
+	"github.com/peterbourgon/ff/v3"
 	"github.com/rode/rode/common"
 )
 
@@ -49,7 +50,7 @@ func Build(name string, args []string) (*Config, error) {
 	flags.StringVar(&c.HarborConfig.Password, "harbor-password", "", "The password to use to authenticate to Harbor")
 	flags.BoolVar(&c.HarborConfig.Insecure, "harbor-insecure", false, "when set, the collector will not verify the TLS certificate for harbor")
 
-	err := flags.Parse(args)
+	err := ff.Parse(flags, args)
 	if err != nil {
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/jarcoal/httpmock v1.0.7
 	github.com/onsi/ginkgo v1.16.2
 	github.com/onsi/gomega v1.12.0
+	github.com/peterbourgon/ff/v3 v3.1.0
 	github.com/rode/rode v0.14.1
 	go.uber.org/zap v1.16.0
-	google.golang.org/grpc v1.37.0
 	google.golang.org/protobuf v1.26.0
 )
